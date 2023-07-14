c4d511272522e29c62ab2531c8e2fefb9732a8ba
David <daveman@gmail.com>
2023-07-13 17:13:11 -0700

    completed step 2

--- 20.1 Simon Game Challenge/Simon Game Challenge Starting Files/game.js
+++ 20.1 Simon Game Challenge/Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,10 @@
-$("h1")
+let buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  console.log(randomChosenColor); //development only
+  gamePattern.push(randomChosenColor);
+  console.log(gamePattern); //development only
+}
