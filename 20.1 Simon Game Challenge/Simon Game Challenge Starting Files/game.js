e7bdbedfe359bc40db68f26c34ee5b9cdb3d768e
David <daveman@gmail.com>
2023-07-13 17:50:17 -0700

    completed step 3

--- 20.1 Simon Game Challenge/Simon Game Challenge Starting Files/game.js
+++ 20.1 Simon Game Challenge/Simon Game Challenge Starting Files/game.js
@@ -5,6 +5,18 @@ function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
   console.log(randomChosenColor); //development only
+
   gamePattern.push(randomChosenColor);
   console.log(gamePattern); //development only
+
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100).on("click", function () {
+    let buttonSound = new Audio("./sounds/" + randomChosenColor + ".mp3");
+    buttonSound.play();
+  });
 }
+
+nextSequence();
+
+$(".btn").on("click", function (event) {
+
+});
