bf7b04397f0f5dae0bc499048e82dcf0c95ec34e
David <daveman@gmail.com>
2023-07-17 15:11:21 -0700

    done with newsletter

--- 25.10 Newsletter Signup/app.js
+++ 25.10 Newsletter Signup/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -12,11 +13,59 @@ app.get("/", function (req, res) {
 })
 
 app.post("/", function (req, res) {
-  let { firstName, lastName, email } = req.body;
+  const { firstName, lastName, email } = req.body;
 
   console.log(firstName, lastName, email);
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/378e01854d";
+
+  const options = {
+    method: "POST",
+    auth: "peterw:068b3c0ab2c1a7b23ef8cb53148d624e-us21"
+    }
+
+  const request = https.request(url, options, function (response) {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+})
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
 })
 
 app.listen(3000, function () {
   console.log("Listening on port 3000");
 });
+
+// API key
+// 068b3c0ab2c1a7b23ef8cb53148d624e-us21
+
+// list ID
+// 378e01854d
