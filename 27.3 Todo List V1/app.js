062f0ddffc7af3adcbd4498e9cc0c07a612968f9
David <daveman@gmail.com>
2023-07-19 14:47:00 -0700

    add switch statements for day of week

--- 27.3 Todo List V1/app.js
+++ 27.3 Todo List V1/app.js
@@ -3,10 +3,47 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-  res.send("hello");
-})
+
+  let today = new Date();
+  let currentDay = today.getDay();
+  let day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+  }
+
+  // if (currentDay === 6 || currentDay === 0) {
+  //   day = "Weekend";
+  // } else {
+  //   day = "Weekday";
+  // }
+
+  res.render("list", { kindOfDay: day });
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port 3000");
-})
+});
