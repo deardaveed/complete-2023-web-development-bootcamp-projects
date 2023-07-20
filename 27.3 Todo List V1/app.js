f708fbc725757c3e6ec30718ed434f6328d3c475
David <daveman@gmail.com>
2023-07-19 17:32:06 -0700

    add work list and separate header and footer into separate files

--- 27.3 Todo List V1/app.js
+++ 27.3 Todo List V1/app.js
@@ -4,10 +4,12 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
@@ -58,7 +60,7 @@ app.get("/", function (req, res) {
   // }
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 });
@@ -66,9 +68,27 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
   let item = req.body.newItem;
 
-  items.push(item);
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 
-  res.redirect("/");
+})
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+})
+
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+
+  workItems.push(item);
+
+  res.redirect("/work");
+
 })
 
 app.listen(process.env.PORT || 3000, function () {
