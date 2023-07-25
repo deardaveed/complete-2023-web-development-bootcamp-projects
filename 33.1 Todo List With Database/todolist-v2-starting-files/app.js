c5a69e19c4691f7b637ad20f66094ebd7d543b47
David <daveman@gmail.com>
2023-07-24 22:11:15 -0700

    update code to use new way of using find() using Promises

--- 33.1 Todo List With Database/todolist-v2-starting-files/app.js
+++ 33.1 Todo List With Database/todolist-v2-starting-files/app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,38 +11,80 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
-
-app.get("/", function(req, res) {
-
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
+//setup to connect MongooseDB:
+main().catch(function (err) {
+  console.log(err);
 });
 
-app.post("/", function(req, res){
+//no longer added to add 2nd parameter to connect method, {usNewUrlParser}
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-  const item = req.body.newItem;
+  // 1. define schema
+  const itemsSchema = new mongoose.Schema({
+    name: String
+  });
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
+  // 2. create new Model based on above schema
+  const Item = mongoose.model("Item", itemsSchema);
+
+  // 3. create new document based on above
+  const item1 = new Item({
+    name: "Exciting todo list item #1!"
+  });
+
+  const item2 = new Item({
+    name: "Exciting todo list item #2!"
+  });
+
+  const item3 = new Item({
+    name: "Exciting todo list item #3!"
+  });
+
+  const defaultItems = [item1, item2, item3];
+
+  try {
+    await Item.insertMany(defaultItems);
+    console.log("Items entered successfully");
+  } catch (err) {
+    console.error("Error inserting items!", err);
   }
-});
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
+  app.get("/", function (req, res) {
 
-app.get("/about", function(req, res){
-  res.render("about");
-});
+    try {
+      Item.find({}).then(function (foundItems) {
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+        res.render("list", { listTitle: "Today", newListItems: foundItems });
+      })
+    } catch (err) {
+      console.error("Not found!", err);
+    }
+
+  });
+
+  app.post("/", function (req, res) {
+
+    const item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+      workItems.push(item);
+      res.redirect("/work");
+    } else {
+      items.push(item);
+      res.redirect("/");
+    }
+  });
+
+  app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+  });
+
+  app.get("/about", function (req, res) {
+    res.render("about");
+  });
+
+  app.listen(3000, function () {
+    console.log("Server started on port 3000");
+  });
+}
