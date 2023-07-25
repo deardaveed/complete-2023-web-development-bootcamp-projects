c6145b28c2825e58b043bcea0af3d7566ea07b80
David <daveman@gmail.com>
2023-07-25 01:48:30 -0700

    refactor for adding todo items to their respective lists

--- 33.1 Todo List With Database/todolist-v2-starting-files/app.js
+++ 33.1 Todo List With Database/todolist-v2-starting-files/app.js
@@ -96,21 +96,27 @@ async function main() {
         res.redirect("/" + customListName);
       }
     })
-
   });
 
   app.post("/", function (req, res) {
 
     const itemName = req.body.newItem;
+    const listName = req.body.list;
 
     const item = new Item({
       name: itemName
     })
 
-    item.save();
-
-    console.log(item);
-    res.redirect("/");
+    if (listName === "Today") {
+      item.save();
+      res.redirect("/");
+    } else {
+      List.findOne({ name: listName }).then(function (foundList) {
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/" + listName);
+      })
+    }
 
   });
 
