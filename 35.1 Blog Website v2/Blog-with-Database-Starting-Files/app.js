0efe60ad385c5610a84cdb0942f62b5f462e77a0
David <daveman@gmail.com>
2023-07-27 19:24:59 -0700

    complete step 2

--- 35.1 Blog Website v2/Blog-with-Database-Starting-Files/app.js
+++ 35.1 Blog Website v2/Blog-with-Database-Starting-Files/app.js
@@ -6,6 +6,32 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// create Schema to define Model for blog post(s)
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+// create a new instance of a Post model based on the defined schema
+const Post = mongoose.model("Post", postSchema);
+
+
 // setup Mongoose DB connection
 main().catch(function (err) {
   console.log(err);
@@ -13,84 +39,66 @@ main().catch(function (err) {
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
+
+  app.get("/", async function (req, res) {
+
+    await Post.find({}).then(function (foundPosts) {
+      if (foundPosts) {
+        console.log("AM I GETTING HIT?");
+        res.render("home", {
+          startingContent: homeStartingContent,
+          posts: foundPosts
+        });
+      } else {
+        console.error("Error getting blog posts!");
+      }
+    })
+
+  });
+
+  app.get("/about", function(req, res){
+    res.render("about", {aboutContent: aboutContent});
+  });
+
+  app.get("/contact", function(req, res){
+    res.render("contact", {contactContent: contactContent});
+  });
+
+  app.get("/compose", function(req, res){
+    res.render("compose");
+  });
+
+  app.post("/compose", async function (req, res) {
+
+    // create new document and insert into 'posts' collection
+    const post = new Post({
+      title: req.body.postTitle,
+      content: req.body.postBody
+    })
+
+    await post.save().then(function (postSaved) {
+
+      if (!postSaved) {
+
+        console.error("ERROR IN SAVING POST", postSaved);
+
+      } else {
+
+        res.redirect("/");
+
+      }
+    })
+
+  })
+
+  app.get("/posts/:postId", function (req, res) {
+    const requestedPostId = req.params.postId;
+    const requestedTitle = _.lowerCase(req.params.postName);
+
+  });
+
+  app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
+
 }
-
-  // create Schema to define Model for blog post(s)
-  const postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-  })
-
-  // create a new instance of a Post model based on the defined schema
-  const Post = mongoose.model("Post", postSchema);
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-const app = express();
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-
-let posts = [];
-
-app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
-  });
-
-});
-
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
-});
-
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
-});
-
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
-
-app.post("/compose", function (req, res) {
-
-  // create new document using instance of Post model
-  const post = new Post({
-    title: req.body.postTitle,
-    content: req.body.postBody
-  })
-
-  post.save();
-
-  console.log(post);
-
-  res.redirect("/");
-
-});
-
-app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
-  });
-
-});
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
