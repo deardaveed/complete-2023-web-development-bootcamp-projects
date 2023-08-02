64fd3dfa54055c846e216bb9996c25a596a15380
David <daveman@gmail.com>
2023-08-02 13:38:22 -0700

    update using dotenv package and variables

--- 37.2 Secrets/app.js
+++ 37.2 Secrets/app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -19,12 +20,9 @@ const userSchema = new mongoose.Schema({
   password: String
 })
 
-const secret = "littlesecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
-
-
 
 app.use(express.static("public"));
 app.set('view engine', "ejs");
