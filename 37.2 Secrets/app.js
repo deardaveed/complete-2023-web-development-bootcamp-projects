92817825c15b48784d790e5197def722c28dbbbc
David <daveman@gmail.com>
2023-08-02 01:36:07 -0700

    implement level 0 auth

--- 37.2 Secrets/app.js
+++ 37.2 Secrets/app.js
@@ -1,1 +1,74 @@
 //jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const app = express();
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/userDB");
+}
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+})
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+app.use(express.static("public"));
+app.set('view engine', "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home");
+})
+
+app.get("/login", (req, res) => {
+  res.render("login");
+})
+
+app.get("/register", (req, res) => {
+  res.render("register");
+})
+
+app.post("/register", async (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  })
+
+  const user = await newUser.save();
+
+  if (user) {
+    res.render("secrets");
+  } else {
+    console.log("error!");
+  }
+
+})
+
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.render("secrets");
+      }
+    } else {
+      console.log("rejected!");
+    }
+  })
+})
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+})
