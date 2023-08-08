f41a2d47e6b03a1e15265de7cb85d6cb8337d123
David <daveman@gmail.com>
2023-08-08 00:40:08 -0700

    initialize and complete short project

--- 38 Keeper App Part 1/keeper-app-part-1-starting/src/index.js
+++ 38 Keeper App Part 1/keeper-app-part-1-starting/src/index.js
@@ -2,7 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import Note from "./components/Note";
 
 ReactDOM.render(
 	<App />,
---
+++ 38 Keeper App Part 2/keeper-app-part-2-starting/src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function createNote(notes) {
+  return <Note
+    name={notes.title}
+    content={notes.content}
+  />
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(createNote)}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
